a0f6db61e60f40c2c162d04b6624cd543921cd19
iqbalkurniawan934 <iqbalkurniawan934@gmail.com>
2023-05-31 21:38:30 +0700

    finish project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Expo Router Example
+
+Use [`expo-router`](https://expo.github.io/router) to build native navigation using files in the `app/` directory.
+
+## 🚀 How to use
+
+```sh
+npx create-expo-app -e with-router
+```
+
+## 📝 Notes
+- [Expo Router: Docs](https://expo.github.io/router)
+- [Expo Router: Repo](https://github.com/expo/router)
+
+Rapid API Jsearch to get API Key in here https://rapidapi.com/letscrape-6bRBa3QguO5/api/jsearch
---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { checkImageURL } from '../../../../utils'
+
+import styles from './nearbyjobcard.style'
+
+const NearbyJobCard = ({ job, handleNavigate }) => {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={handleNavigate}
+    >
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image
+          source={{ uri: checkImageURL(job.employer_logo) ? job.employer_logo: 'https://img.freepik.com/premium-vector/go-startup-logo_8251-75.jpg?w=2000' }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>{job.job_title}</Text>
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default NearbyJobCard
